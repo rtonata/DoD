94b0fd70da80713cd453648a610f03aff9fcddaf
Richard Chambula <Richard.Chambula@students.newport.ac.uk>
2013-05-25 16:49:31 +0100

    Added a new class. The class Gets and Sets the items that are found at various locations in the world, thus, all locations hosting items must make a reference to this class. Similarly, when an avatar is holding an item, it should be a reference to this class.

---
+++ src/my/newport/ac/uk/Item.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.newport.ac.uk;
+
+/**
+ * Gets and Sets the items that are found at various locations in the world
+ * @author Eyoka
+ */
+public class Item {
+    
+    private String itemName;//Holds the name of the item
+
+    /**
+     * Default constructor 
+     * @param itemName the name of the item
+     */
+    public Item(String itemName) {
+        this.itemName = itemName;
+    }
+
+    /**
+     * Gets the name of the item
+     * @return the name of the item
+     */
+    public String getItemName() {
+        return itemName;
+    }
+    
+}
