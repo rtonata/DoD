8c5888a663e7d8906ab4065536321818146c3d04
Richard Chambula <Richard.Chambula@students.newport.ac.uk>
2013-05-25 16:10:07 +0100

    This is the first commit, the game still lack some functionality. At this point, the avatar has limited commands, he/she can only navigate between different locations found in the world.

---
+++ src/my/newport/ac/uk/Player.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.newport.ac.uk;
+
+/**
+ *
+ * @author tdahl01
+ */
+public class Player {
+    
+    Location location;
+    
+    Player(Location start){
+        location = start;
+    }
+    
+}
